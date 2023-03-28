513e85a280ada4c4d28f76a44f267b0428a0a687
Vajira Attanayake <vajiraatt@gmail.com>
2023-03-28 15:48:44 +1100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>Learn React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
