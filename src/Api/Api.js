54193ce8f08ce0ee8acb8abfeb73bd67897a912f
Vajira Attanayake <vajiraatt@gmail.com>
2023-03-31 01:56:50 +1100

    Added API class

---
+++ src/Api/Api.js
@@ -0,0 +1,39 @@
+class API {
+  constructor() {
+    this.base_url = "http://127.0.0.1:8000";
+  }
+
+  async checkConnection() {
+    const path = "/";
+    const url = this.base_url + path;
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getSpotifyURL() {
+    const path = "/authorise";
+    const url = this.base_url + path;
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export default API;
