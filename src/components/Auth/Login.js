b1a772606c7ee01e5b4036eaab6ff227c98377eb
Vajira Attanayake <vajiraatt@gmail.com>
2023-03-31 01:57:23 +1100

    Changed login function to async to get request

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,21 @@
 import "./Login.css";
 import LoginButton from "../UI/LoginButton";
+import API from "../../Api/Api";
+import { useState } from "react";
 
 const Login = () => {
-  const spotifyAuthenticateHandler = () => {};
+  const apiInstance = new API();
+  async function spotifyAuthenticateHandler() {
+    try {
+      const response = await apiInstance.getSpotifyURL();
+      const url = response.result;
+      console.log(url);
+      window.location.href = url;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="login">
       <header className="login-header">
